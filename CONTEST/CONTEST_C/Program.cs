7a223a97807fa6c248f888053ca0da9486ba9653
EHOTIK911 <42716046+EHOTIK911@users.noreply.github.com>
2021-11-10 13:34:27 +0300

    up

--- CONTEST/CONTEST_C/Program.cs
+++ CONTEST/CONTEST_C/Program.cs
@@ -11,7 +11,8 @@ namespace CONTEST_C
         static void Main(string[] args)
         {
             int s = int.Parse(Console.ReadLine());
-            List<int> MAS = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(i => int.Parse(i)).ToList(); MAS.Add(0);
+            List<int> MAS = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(i => int.Parse(i)).ToList(); 
+            MAS.Add(0);
             var result = MAS.OrderByDescending(x => x).Select((x, i) => x + i).Min();
             Console.WriteLine(result);
 
